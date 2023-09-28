5110cb40582e7826ec1c8dd337c29d35cc55c74c
hongyee34 <baram641113@gmail.com>
2023-09-28 14:58:32 +0900

    비동기 송수신

--- XOX_Server/XOX_Server/Program.cs
+++ XOX_Server/XOX_Server/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace XOX_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server().Wait();
+        }
+        async static Task Server()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Parse("192.168.0.15"), 6974);
+            listener.Start();
+            Console.WriteLine("서버 실행중");
+            while (true)
+            {
+                // 비동기 Accept                
+                TcpClient client = await listener.AcceptTcpClientAsync();
+                Console.WriteLine("새 클라이언트 연결됨");
+
+                HandleClient(client);
+            }
+        }
+        async static void HandleClient(TcpClient client)
+        {
+            const int MAX_SIZE = 1024;
+            NetworkStream stream = client.GetStream();
+
+            // 비동기 수신            
+            byte[] buff = new byte[MAX_SIZE];
+            int nbytes = await stream.ReadAsync(buff, 0, buff.Length);
+            if (nbytes > 0)
+            {
+                string msg = Encoding.ASCII.GetString(buff, 0, nbytes);
+                Console.WriteLine($"{msg} at {DateTime.Now}");
+
+                // 비동기 송신
+                await stream.WriteAsync(buff, 0, nbytes);
+            }
+
+            stream.Close();
+            client.Close();
+        }
+    }
+}
